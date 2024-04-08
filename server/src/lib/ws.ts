5958963087b251de1ceca5ee68db331914f77ecb
Pedro Viana <pedroviana.developer13@gmail.com>
2024-04-08 18:28:28 -0300

    core: config mediasoup

--- server/src/lib/ws.ts
+++ server/src/lib/ws.ts
@@ -1,6 +1,15 @@
 import WebSocket from "ws";
+import { createWorker } from "./worker";
+
+let mediasoupRouter;
 
 const WebSocketConnection = async (websock: WebSocket.Server) => {
+    try {
+        mediasoupRouter = await createWorker();
+    } catch (error) {
+        throw error;
+    }
+
     websock.on('connection', (ws: WebSocket) => {
 
         ws.on('message', (message: string) => {
